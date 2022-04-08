ff02f9205ae87bbe05f62f898ff296fbf7954943
Muhammad Ismail <60970076+Muhammad0302@users.noreply.github.com>
2022-04-08 21:41:01 +0500

    changes added

--- index.js
+++ index.js
@@ -39,8 +39,8 @@ app.use(express.json());
 app.use(fileUpload());
 app.use(express.static("./public"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/userAuth", userAuthRoute);
 app.use("/api/adminAuth", adminAuthRoute);
