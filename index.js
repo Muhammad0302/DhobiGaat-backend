7c532df9acad8c5398afadac96dfe995a6f48a2a
Muhammad Ismail <60970076+Muhammad0302@users.noreply.github.com>
2022-04-08 21:54:29 +0500

    changes added

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(express.static("./public"));
 
