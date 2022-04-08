07eb9baa1d803993b0515496b4cb0a17edd1b4d5
Muhammad Ismail <60970076+Muhammad0302@users.noreply.github.com>
2022-04-08 21:43:49 +0500

    changes added

--- index.js
+++ index.js
@@ -30,17 +30,15 @@ mongoose
   .catch((err) => {
     console.error(err);
   });
-app.use(express.urlencoded({ extended: false }));
-
 app.use(cookieParser());
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 app.use(express.static("./public"));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api/userAuth", userAuthRoute);
 app.use("/api/adminAuth", adminAuthRoute);
